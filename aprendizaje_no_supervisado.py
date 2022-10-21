6bbb8a61a20f94b9d7b65686cb2b842fb387b410
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-10-21 18:11:01 +0200

    requerimientos de librerias

--- aprendizaje_no_supervisado.py
+++ aprendizaje_no_supervisado.py
@@ -20,71 +20,71 @@
 
 
 
-import pandas as pnd
-import matplotlib.pyplot as plt
+# import pandas as pnd
+# import matplotlib.pyplot as plt
 
 
-#Caraga de datos
-#frutas = pnd.read_csv("datas/frutas.csv", names=['DIAMETRO','PESO'], header=None)
+# #Caraga de datos
+# #frutas = pnd.read_csv("datas/frutas.csv", names=['DIAMETRO','PESO'], header=None)
 
-# Visualización gráfica de los datos
-# frutas.plot.scatter(x="DIAMETRO",y="PESO")
-# plt.show()
-
-
-# #Aprendizaje con el algoritmo K-Mean
-# from sklearn.cluster import KMeans
-# modelo=KMeans(n_clusters=2)
-# modelo.fit(frutas)
-
-# #Predicciones
-# predicciones_kmeans = modelo.predict(frutas)
-
-# #Visualización de la clusterización
-# plt.scatter(frutas.DIAMETRO, frutas.PESO, c=predicciones_kmeans, s=50, cmap='viridis')
-# plt.xlabel("DIAMETRO")
-# plt.ylabel("PESO")
-
-#Visualización de los centroides
-#centers = modelo.cluster_centers_
-#plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
-#plt.show()
-
-#Guardar el modelo (eliminar marca de comentario # si es necesario)
-#from joblib import dump
-#dump(modelo,'modelos/kmean.joblib')
-
-#--- Realización de las clasificaciones --
-#CEREZA: 26,98 mm de diametro ,8,75 gramos
-#ALBARICOQUE: 55,7  mm de diámetro , 102,16 gramos
+# # Visualización gráfica de los datos
+# # frutas.plot.scatter(x="DIAMETRO",y="PESO")
+# # plt.show()
 
 
-cereza = [[26.98,8.75]]
-numCluster = modelo.predict(cereza)
-print("Número de clúster de las cerezas: "+ str(numCluster))
+# # #Aprendizaje con el algoritmo K-Mean
+# # from sklearn.cluster import KMeans
+# # modelo=KMeans(n_clusters=2)
+# # modelo.fit(frutas)
+
+# # #Predicciones
+# # predicciones_kmeans = modelo.predict(frutas)
+
+# # #Visualización de la clusterización
+# # plt.scatter(frutas.DIAMETRO, frutas.PESO, c=predicciones_kmeans, s=50, cmap='viridis')
+# # plt.xlabel("DIAMETRO")
+# # plt.ylabel("PESO")
+
+# #Visualización de los centroides
+# #centers = modelo.cluster_centers_
+# #plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
+# #plt.show()
+
+# #Guardar el modelo (eliminar marca de comentario # si es necesario)
+# #from joblib import dump
+# #dump(modelo,'modelos/kmean.joblib')
+
+# #--- Realización de las clasificaciones --
+# #CEREZA: 26,98 mm de diametro ,8,75 gramos
+# #ALBARICOQUE: 55,7  mm de diámetro , 102,16 gramos
 
 
-albaricoque = [[55.7,102.16]]
-numCluster = modelo.predict(albaricoque)
-print("Número de clúster de los albaricoques: " + str(numCluster))
-
-#Instrucciones a adaptar en función de los números de clústeres
-#determinados con anterioridad:
-
-cereza = [[26.98,8.75]]
-numCluster = modelo.predict(cereza)
-if int(numCluster)==0:
-    print("¡Es un albaricoque!")
-else:
-    print("¡Es una cereza! ")
+# cereza = [[26.98,8.75]]
+# numCluster = modelo.predict(cereza)
+# print("Número de clúster de las cerezas: "+ str(numCluster))
 
 
-albaricoque = [[55.7,102.16]]
-numCluster = modelo.predict(albaricoque)
-if int(numCluster)==0:
-    print("¡Es un albaricoque!")
-else:
-    print("¡Es una cereza!")
+# albaricoque = [[55.7,102.16]]
+# numCluster = modelo.predict(albaricoque)
+# print("Número de clúster de los albaricoques: " + str(numCluster))
+
+# #Instrucciones a adaptar en función de los números de clústeres
+# #determinados con anterioridad:
+
+# cereza = [[26.98,8.75]]
+# numCluster = modelo.predict(cereza)
+# if int(numCluster)==0:
+#     print("¡Es un albaricoque!")
+# else:
+#     print("¡Es una cereza! ")
+
+
+# albaricoque = [[55.7,102.16]]
+# numCluster = modelo.predict(albaricoque)
+# if int(numCluster)==0:
+#     print("¡Es un albaricoque!")
+# else:
+#     print("¡Es una cereza!")
 
 
 #---- Modelo de mezclas Gaussianas (GMM) -----------
