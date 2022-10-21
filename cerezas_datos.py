d41b79807b80c8baa75efdd738d03062d0e8b1ff
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-10-21 11:18:45 +0200

    poo en los archivos de las frutas

--- cerezas_datos.py
+++ cerezas_datos.py
@@ -1,15 +1,26 @@
 import numpy as np
 
-class datos_cerezas:
+class Cereza:
 
-    def __init__(self,cantidadObservaciones):
-        self.cantidadObservaciones = cantidadObservaciones
+    def __init__(self, diametro, peso):
+        self.cerezas_lista=[]
+        self.diametro = diametro
+        self.peso = peso
 
-    def set_cerezas(self, cereza):
-        self.cereza=cereza
+    def __str__(self):
+        return f"({self.diametro}) {self.peso}"
+class datos_cerezas(Cereza):
+
+    def __init__(self, diametro, peso, cantidadObservaciones, cerezas_lista):
+        super().__init__(diametro, peso)
+        self.cantidadObservaciones=cantidadObservaciones
+        self.cerezas_lista=cerezas_lista
+
+    def set_cerezas(self, cerezas_lista):
+        self.cerezas_lista=cerezas_lista
 
     def get_cerezas(self):
-        print('Medidas de cerezas: '+self.cereza)
+        print('Medidas de cerezas: '+self.cerezas_lista)
 
     def generar_cerezas(self):
         #GENERACION DE LOS DATOS
@@ -18,7 +29,6 @@ class datos_cerezas:
 
         #Generación de las cerezas
         caracteristicasCerezas = [[17,19,1,5],[20,21,5,6],[22,23,6,7],[24,25,7,8.5],[26,27,8.5,10],[28,29,10,11.5]]
-        cerezas_lista = []
         np.random.seed()
         for iteration in range(self.cantidadObservaciones):
             #elección al azar de una característica
@@ -28,6 +38,6 @@ class datos_cerezas:
             #Generación de un peso
             peso = np.round(np.random.uniform(cereza[2], cereza[3]),2)
             print ("Cereza "+str(iteration)+" "+str(cereza)+" : "+str(diametro)+" - "+str(peso))
-            cerezas_lista.append([diametro,peso])
+            self.cerezas_lista.append([diametro,peso])
 
-        return cerezas_lista
+        return self.cerezas_lista
