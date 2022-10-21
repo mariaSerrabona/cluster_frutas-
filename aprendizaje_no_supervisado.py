1fa4379eafd81ccb45f2964e38ee508c29a3cb36
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-10-21 15:48:23 +0200

    poo en cluster y nuevo archivo de prediccion

--- aprendizaje_no_supervisado.py
+++ aprendizaje_no_supervisado.py
@@ -25,30 +25,30 @@ import matplotlib.pyplot as plt
 
 
 #Caraga de datos
-frutas = pnd.read_csv("datas/frutas.csv", names=['DIAMETRO','PESO'], header=None)
+#frutas = pnd.read_csv("datas/frutas.csv", names=['DIAMETRO','PESO'], header=None)
 
-#Visualización gráfica de los datos
-frutas.plot.scatter(x="DIAMETRO",y="PESO")
-plt.show()
+# Visualización gráfica de los datos
+# frutas.plot.scatter(x="DIAMETRO",y="PESO")
+# plt.show()
 
 
-#Aprendizaje con el algoritmo K-Mean
-from sklearn.cluster import KMeans
-modelo=KMeans(n_clusters=2)
-modelo.fit(frutas)
+# #Aprendizaje con el algoritmo K-Mean
+# from sklearn.cluster import KMeans
+# modelo=KMeans(n_clusters=2)
+# modelo.fit(frutas)
 
-#Predicciones
-predicciones_kmeans = modelo.predict(frutas)
+# #Predicciones
+# predicciones_kmeans = modelo.predict(frutas)
 
-#Visualización de la clusterización
-plt.scatter(frutas.DIAMETRO, frutas.PESO, c=predicciones_kmeans, s=50, cmap='viridis')
-plt.xlabel("DIAMETRO")
-plt.ylabel("PESO")
+# #Visualización de la clusterización
+# plt.scatter(frutas.DIAMETRO, frutas.PESO, c=predicciones_kmeans, s=50, cmap='viridis')
+# plt.xlabel("DIAMETRO")
+# plt.ylabel("PESO")
 
 #Visualización de los centroides
-centers = modelo.cluster_centers_
-plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
-plt.show()
+#centers = modelo.cluster_centers_
+#plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
+#plt.show()
 
 #Guardar el modelo (eliminar marca de comentario # si es necesario)
 #from joblib import dump
--- creador_cluster.py
+++ creador_cluster.py
@@ -1,55 +1,40 @@
-from dataclasses import dataclass
 import pandas as pd
-import numpy as np
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+
 
 class cluster():
 
     #constructor de la clase
-    def __init__(self,datos, num_cluster):
-        self.datos = datos
+    def __init__(self,datos_frutas, num_cluster):
+        self.datos_frutas = datos_frutas
         self.num_cluster=num_cluster
 
 
     #leemos los datos
     def leer_datos(self):
-        self.datos=pd.reaad_csv('datas/frutas.csv',names=['DIAMETRO','PESO'], header=None )
+        self.datos_frutas=pd.read_csv('datas/frutas.csv',names=['DIAMETRO','PESO'], header=None )
 
     def generar_guardar_plot(self):
         #Visualización gráfica de los datos
-        self.datos.plot.scatter(x='DIAMETRO',y='PESO')
+        self.datos_frutas.plot.scatter(x='DIAMETRO',y='PESO')
         #gaurdamos el plot en un archivo
         plt.savefig('scatter_frutas.png')
 
+    def generar_cluster(self):
+        #Aprendizaje con el algoritmo K-Mean
+        modelo=KMeans(self.num_cluster)
+        modelo.fit(self.datos_frutas)
+        #Predicciones
+        predicciones_kmeans = modelo.predict(self.datos_frutas)
 
+        #Visualización de la clusterización
+        plt.scatter(self.datos_frutas.DIAMETRO, self.datos_frutas.PESO, c=predicciones_kmeans, s=50, cmap='viridis')
+        plt.xlabel("DIAMETRO")
+        plt.ylabel("PESO")
 
+        #Visualización de los centroides
+        centers = modelo.cluster_centers_
+        plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
+        plt.savefig('KMeans_frutas.png')
 
-
-
-
-
-    #Caraga de datos
-
-#Visualización gráfica de los datos
-frutas.plot.scatter(x="DIAMETRO",y="PESO")
-plt.show()
-
-
-#Aprendizaje con el algoritmo K-Mean
-from sklearn.cluster import KMeans
-modelo=KMeans(n_clusters=2)
-modelo.fit(frutas)
-
-#Predicciones
-predicciones_kmeans = modelo.predict(frutas)
-
-#Visualización de la clusterización
-plt.scatter(frutas.DIAMETRO, frutas.PESO, c=predicciones_kmeans, s=50, cmap='viridis')
-plt.xlabel("DIAMETRO")
-plt.ylabel("PESO")
-
-#Visualización de los centroides
-centers = modelo.cluster_centers_
-plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
-plt.show()
